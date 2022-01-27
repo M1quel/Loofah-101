966eaed61c320b8aa5ccd97108301893b30a93f2
Mikkel Frederiksen <mikkelatterup@gmail.com>
2022-01-27 21:41:32 +0100

    fixed another warning

--- src/components/workoutCard/WorkoutCard.js
+++ src/components/workoutCard/WorkoutCard.js
@@ -14,6 +14,7 @@ export default function Workoutcard(props) {
                 setImageSrc(url);
             })
         }
+        // eslint-disable-next-line
     }, [props.imgSrc])
 
     return (
