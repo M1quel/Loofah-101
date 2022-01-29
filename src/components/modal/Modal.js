2bd523c5dc3073244207a4a69ba653d7714a83fe
Mikkel Frederiksen <mikkelatterup@gmail.com>
2022-01-29 11:42:27 +0100

    begun edit and delete modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import "./Modal.scss";
 import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react/cjs/react.development';
 
 export default function Modal(props) {
+    var [mode, setMode] = useState("view");
     
     function handleExit (e) {
         if (e.target.classList.contains("overlay")) {
@@ -15,15 +17,63 @@ export default function Modal(props) {
             <motion.div 
             className='overlay' 
             onClick={(e) => handleExit(e)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            transition={{ duration: 0.15}}
             >
                 <motion.div 
                 initial={{y: "100%"}}
                 animate={{y: 0}}
                 exit={{y: "100%"}}
-                transition={{duration: 0.2, ease: 'easeOut'}}
-                className='overlat__content'>
-                    {props.children}
+                transition={{duration: 0.15, ease: 'easeOut'}}
+                className='overlay__content'>
+                    <AnimatePresence initial={false}>
+                        <AnimatePresence>
+                            {mode == "view" && <motion.div 
+                                className='overlay__content--view'
+                                initial={{x: "-200%"}}
+                                animate={{x: 0}}
+                                exit={{x: "-200%"}}
+                                transition={{duration: 0.2}}
+                            >
+                                {props.children}
+
+                                <div className='modeButtonsWrapper'>
+                                    <button className='modeButtons' onClick={() => setMode("edit")}>
+                                        Edit
+                                    </button>
+                                    <button className='modeButtons'>
+                                        Delete
+                                    </button>
+                                </div>
+
+                            </motion.div>}
+                        </AnimatePresence>
+                        <AnimatePresence>
+                            {mode == "edit" && <motion.div
+                                className='overlay__content--edit'
+                                initial={{x: "200%"}}
+                                animate={{x: 0}}
+                                exit={{x: "200%"}}
+                                transition={{duration: 0.2}}
+                            >
+                                {props.children}
+                                <div className='modeButtonsWrapper'>
+                                    <button className='modeButtons' onClick={() => setMode("view")}>
+                                        asdasd
+                                    </button>
+                                    <button className='modeButtons'>
+                                        Delete
+                                    </button>
+                                </div>
+                                
+                            </motion.div>}
+                        </AnimatePresence>
+
+                    </AnimatePresence>
                 </motion.div>
+                
             </motion.div>
         </>
     )
